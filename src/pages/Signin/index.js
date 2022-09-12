39cf86d88ff8a3c244c6244da7058d564277fad9
mykael-paixao <lucas.moura@itriad.org.br>
2022-09-12 17:40:57 -0400

    chore: customize code

--- src/pages/Signin/index.js
+++ src/pages/Signin/index.js
@@ -1,8 +1,9 @@
-import React, { useState } from "react";
-import Input from "../../components/Input";
-import Button from "../../components/Button";
-import * as C from "./styles";
 import { Link, useNavigate } from "react-router-dom";
+import React, { useState } from "react";
+
+import * as C from "./styles";
+import Button from "../../components/Button";
+import Input from "../../components/Input";
 import useAuth from "../../hooks/useAuth";
 
 const Signin = () => {
@@ -52,6 +53,11 @@ const Signin = () => {
             <Link to="/signup">&nbsp;Registre-se</Link>
           </C.Strong>
         </C.LabelSignup>
+        <C.LabelRemember>
+          <C.Strong>
+          <Link to="/remember">&nbsp;Esqueceu sua senha?</Link>
+          </C.Strong>
+        </C.LabelRemember>
       </C.Content>
     </C.Container>
   );
