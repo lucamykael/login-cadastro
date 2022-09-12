ceb9c123511f8da303a5ce4394096e165ec48d55
mykael-paixao <lucas.moura@itriad.org.br>
2022-09-12 17:38:08 -0400

    style: customize page to Remember password

---
+++ src/pages/Remember/styles.js
@@ -0,0 +1,56 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  gap: 10px;
+  height: 100vh;
+  background-image: linear-gradient(#100a2f, #25023c);
+`;
+
+export const Content = styled.div`
+  gap: 15px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  width: 100%;
+  box-shadow: 0 1px 2px #0003;
+  background-color: #ff3cac;
+  background-image: linear-gradient(
+    225deg,
+    #ff3cac 0%,
+    #784ba0 50%,
+    #2b86c5 100%
+  );
+  max-width: 350px;
+  padding: 20px;
+  border-radius: 12px;
+`;
+
+export const Label = styled.label`
+  font-size: 22px;
+  font-weight: 600;
+  color: white;
+`;
+
+export const LabelRemember = styled.label`
+  font-size: 16px;
+  color: white;
+`;
+
+export const labelError = styled.label`
+  font-size: 14px;
+  color: red;
+`;
+
+export const Strong = styled.strong`
+  cursor: pointer;
+
+  a {
+    text-decoration: none;
+    color: white;
+  }
+`;
